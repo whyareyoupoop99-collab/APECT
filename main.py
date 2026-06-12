704ed83fa034f1e7fd218e3523393c44b79f6b08
Alim Mustafin <whyareyoupoop99@gmail.com>
2026-06-12 20:50:05 +0600

    Create main.py
    well my first update on the chess engine, hopefully Harvard or MIT see this!

---
+++ main.py
@@ -0,0 +1,6 @@
+print("WELCOME TO MY FIRST PROJECT, THIS IS A CHESS ENGINE WHERE YOU CAN PLAY BY YOURSELF, ENJOY (from Alim Mustafin 11 year old 2026) ") 
+print("INPUT A MOVE (E4 ETC) AND THEN THE BOARD WILL SWITCH SIDES FOR YOU TO PLAY FOR HE OTHER COLOUR") 
+user_move = input("ENTER MOVE HERE:") 
+git add .
+git commit -m "Update APECT terminal loops"
+git push
